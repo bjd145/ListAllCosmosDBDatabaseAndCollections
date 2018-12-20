d286cb57871e685c7f0bf8a454d70dc895fde72e
Brian Denicola <bjd145@yahoo.com>
2018-12-20 10:18:45 -0600

    Updated formatting

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -23,34 +23,38 @@ namespace CosmosDBWithEventualConsistency
 
     public class Program
     {
-		public static async Task WalkCosmosAccounts(IAzure azure)
+        public static async Task WalkCosmosAccounts(IAzure azure)
         {
 
             try
             {
-				foreach (var cosmosDBAccount in azure.CosmosDBAccounts.List()) {
-					var databaseAccountListKeysResult = cosmosDBAccount.ListKeys();
-					string masterKey = databaseAccountListKeysResult.PrimaryMasterKey;
-					string endPoint = cosmosDBAccount.DocumentEndpoint;
+                foreach (var cosmosDBAccount in azure.CosmosDBAccounts.List())
+                {
+                    var databaseAccountListKeysResult = cosmosDBAccount.ListKeys();
+                    string masterKey = databaseAccountListKeysResult.PrimaryMasterKey;
+                    string endPoint = cosmosDBAccount.DocumentEndpoint;
 
-					var client = new DocumentClient(new Uri(endPoint), masterKey, ConnectionPolicy.Default);
-					var databases = await client.ReadDatabaseFeedAsync();
-						
-					Console.WriteLine("Reading all databases resources for - " + cosmosDBAccount.Name);
-					foreach (var db in databases) {
-						List<DocumentCollection> collections = client.CreateDocumentCollectionQuery((String)db.SelfLink).ToList();
-						Console.WriteLine("\tDatabase Name - " + db.Id);
-						foreach (var col in collections) {
-							Console.WriteLine("\t\tCollection Name - " + col.Id);
-						}
-					}
-				}
+                    var client = new DocumentClient(new Uri(endPoint), masterKey, ConnectionPolicy.Default);
+                    var databases = await client.ReadDatabaseFeedAsync();
+
+                    Console.WriteLine("Reading all databases resources for - " + cosmosDBAccount.Name);
+                    foreach (var db in databases)
+                    {
+                        List<DocumentCollection> collections = client.CreateDocumentCollectionQuery((String)db.SelfLink).ToList();
+                        Console.WriteLine("\tDatabase Name - " + db.Id);
+                        foreach (var col in collections)
+                        {
+                            Console.WriteLine("\t\tCollection Name - " + col.Id);
+                        }
+                    }
+                }
             }
-			catch (Exception e) {
-				Utilities.Log(e.Message);
-				Utilities.Log(e.StackTrace);
-			}
-		}
+            catch (Exception e)
+            {
+                Utilities.Log(e.Message);
+                Utilities.Log(e.StackTrace);
+            }
+        }
 
         public static void Main(string[] args)
         {
@@ -59,16 +63,16 @@ namespace CosmosDBWithEventualConsistency
                 var clientId = Environment.GetEnvironmentVariable("AZURE_CLIENTID");
                 var clientSecret = Environment.GetEnvironmentVariable("AZURE_CLIENTSECRET");
                 var tenantId = Environment.GetEnvironmentVariable("AZURE_TENANTID");
-				var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal( clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
+                var credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(clientId, clientSecret, tenantId, AzureEnvironment.AzureGlobalCloud);
 
-				var azure = Azure
+                var azure = Azure
                     .Configure()
                     .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
                     .Authenticate(credentials)
                     .WithDefaultSubscription();
 
-				Utilities.Log("Selected subscription: " + azure.SubscriptionId);
-				WalkCosmosAccounts(azure).Wait();
+                Utilities.Log("Selected subscription: " + azure.SubscriptionId);
+                WalkCosmosAccounts(azure).Wait();
             }
             catch (Exception e)
             {
